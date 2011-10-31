58e857230dc4b4ae651f37efe383da413fd5575e
Walter Nelson <walter.john.nelson@gmail.com>
2011-10-31 12:31:50 -0400

    Completed main Hotel API

--- lib/suitcase/hotel/hotel.rb
+++ lib/suitcase/hotel/hotel.rb
@@ -1,19 +1,43 @@
 require 'net/http'
 require 'uri'
 require 'json'
-require 'api_key'
+require '../country_codes'
+load 'api_key.rb'
 
 module Suitcase
   class Hotel
-    attr_accessor :id, :address, :city, :postal_code, :country, :airport_code, :rating, :confidence_rating, :description, :high_rate, :low_rate, :currency, :latitude, :longitude 
+    attr_accessor :id, :name, :address, :city, :postal_code, :country, :airport_code, :rating, :confidence_rating, :description, :high_rate, :low_rate, :tripadvisor_rating, :currency_code, :latitude, :longitude
 
     def self.near(location, number_of_results)
-      unparsed = Net::HTTP.get_response(URI.parse("http://api.ean.com/ean-services/rs/hotel/v3/list?apiKey=#{API_KEY}&city=#{location}&numberOfResults=#{number_of_results}")).body
+      hotels = []
+      unparsed = Net::HTTP.get_response(URI.parse("http://api.ean.com/ean-services/rs/hotel/v3/list?apiKey=#{API_KEY}&city=#{location}&numberOfResults=#{number_of_results}".gsub!(" ", "%20"))).body
+      puts unparsed
       json = JSON.parse unparsed
-      json["HotelListResponse"]["HotelList"]["HotelSummary"].each do |hotel_data|
-        h = Hotel.new
-        h.id = hotel_data["hotelId"]
-        h.name = hotel_data["name"]
+      if json["HotelListResponse"]["HotelList"]
+        json["HotelListResponse"]["HotelList"]["HotelSummary"].each do |hotel_data|
+          h = Hotel.new
+          h.id = hotel_data["hotelId"]
+          h.name = hotel_data["name"]
+          h.address = hotel_data["address1"]
+          h.city = hotel_data["city"]
+          h.postal_code = hotel_data["postalCode"]
+          h.country = COUNTRY_CODES[hotel_data["countryCode"]]
+          h.airport_code = hotel_data["airportCode"]
+          h.rating = hotel_data["hotelRating"]
+          h.confidence_rating = hotel_data["confidenceRating"]
+          h.tripadvisor_rating = hotel_data["tripAdvisorRating"]
+          h.currency_code = hotel_data["rateCurrencyCode"]
+          h.latitude = hotel_data["latitude"]
+          h.longitude = hotel_data["longitude"]
+          h.high_rate = hotel_data["highRate"]
+          h.low_rate = hotel_data["lowRate"]
+          hotels.push(h)
+        end
+        hotels[0..number_of_results]
+      else
+        if json["HotelListResponse"]["EanWsError"]
+          raise "An error occured. Check data."
+        end
       end
     end
 
@@ -26,6 +50,3 @@ module Suitcase
     end
   end
 end
-
-include Suitcase
-Hotel.near("London", 10)
---
+++ lib/suitcase/hotel/test.rb
@@ -0,0 +1,4 @@
+load 'hotel.rb'
+
+include Suitcase
+Hotel.near('5 Old Common, Millbury, MA', 10)
