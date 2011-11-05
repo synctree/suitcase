e4297786cd272d222baadca6b7a31a27baa1954a
Walter Nelson <walter.john.nelson@gmail.com>
2011-11-04 21:35:50 -0400

    Removed tests

--- lib/suitcase/air/test.rb
+++
@@ -1,5 +0,0 @@
-load 'air.rb'
-
-include Suitcase
-flights = Suitcase::Flight.available(:from => "BOS", :to => "LHR", :departs => "1/7/2012 10:00 AM", :arrives => "1/7/2012 1:00 AM", :adults => 1)
-puts flights
