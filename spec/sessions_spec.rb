96d9f217df6cd846aeb7f7a3b5bfb392dde2fb5e
Walter Nelson <walter.john.nelson@gmail.com>
2012-01-14 20:56:15 -0500

    Wrote sessions specs; starting over

---
+++ spec/sessions_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe Suitcase::Session do
+  it { should respond_to :id }
+  it { should respond_to :ip_address }
+  it { should respond_to :locale }
+  it { should respond_to :currency_code }
+
+  it "should be able to be passed in to a find query" do
+    s = Suitcase::Session.new
+    Suitcase::Hotel.find(id: 123904, session: s)
+    s.id.should_not be_empty
+  end
+end
