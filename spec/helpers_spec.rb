e24699486a21b47e7b138645c8d91e80b549400b
Zack Hovatter <zackhovatter@gmail.com>
2012-04-19 08:38:14 -0400

    write tests for digital signature authentication

--- spec/helpers_spec.rb
+++ spec/helpers_spec.rb
@@ -5,15 +5,36 @@ describe Suitcase::Helpers do
     class Dummy; extend Suitcase::Helpers; end
   end
 
-  it "#url should return a URI with the correct base URL" do
-    returned = Dummy.url(:method => "action", :params => {})
-    returned.should be_a(URI)
-    returned.host.should match(/api.ean.com/)
+  describe "#url" do
+    it "should return a URI with the correct base URL" do
+      returned = Dummy.url(:method => "action", :params => {})
+      returned.should be_a(URI)
+      returned.host.should match(/api.ean.com/)
+    end
+
+    context "when using digital signature authentication" do
+      it "should add a 'sig' parameter" do
+        Suitcase::Configuration.stub(:use_signature_auth?) { true }
+        Dummy.stub(:generate_signature) { "test" }
+
+        returned = Dummy.url(:method => "action", :params => {})
+        returned.query.should match(/sig=test/)
+      end
+    end
   end
 
   it "#parse_response should raise an exception when passed an invalid URI" do
     lambda do
       Dummy.parse_response(URI.parse "http://google.com")
     end.should raise_error
+  end
+
+  it "#generate_signature should return the encrypted api key, shared secret, and timestamp" do
+    Suitcase::Configuration.stub(:hotel_api_key) { "abc" }
+    Suitcase::Configuration.stub(:hotel_shared_secret) { "123" }
+    Time.stub(:now) { "10" }
+
+    returned = Dummy.generate_signature
+    returned.should == Digest::MD5.hexdigest("abc12310")
   end
 end
