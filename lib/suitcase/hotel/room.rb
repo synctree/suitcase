6edccd9f0ffab6c37826035f0703e8a0fa83e646
Walter Nelson <walter.john.nelson@gmail.com>
2012-05-10 22:50:17 -0400

    * Cleaned up the accessors in Room.

--- lib/suitcase/hotel/room.rb
+++ lib/suitcase/hotel/room.rb
@@ -1,6 +1,14 @@
 module Suitcase
   class Room
-    attr_accessor :rate_key, :hotel_id, :supplier_type, :rate_code, :room_type_code, :supplier_type, :tax_rate, :non_refundable, :occupancy, :quoted_occupancy, :min_guest_age, :total, :surcharge_total, :nightly_rate_total, :average_base_rate, :average_rate, :max_nightly_rate, :currency_code, :value_adds, :room_type_description, :price_breakdown, :total_price, :average_nightly_rate, :promo, :arrival, :departure, :rooms, :bed_types
+    attr_accessor :rate_key, :hotel_id, :supplier_type, :rate_code,
+                  :room_type_code, :supplier_type, :tax_rate, :non_refundable,
+                  :occupancy, :quoted_occupancy, :min_guest_age, :total,
+                  :surcharge_total, :nightly_rate_total, :average_base_rate,
+                  :average_rate, :max_nightly_rate, :currency_code, :value_adds,
+                  :room_type_description, :price_breakdown, :total_price,
+                  :average_nightly_rate, :promo, :arrival, :departure, :rooms,
+                  :bed_types
+
     extend Suitcase::Helpers
 
     def initialize(info)
