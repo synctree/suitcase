ff6aa7c87cc0e1c3a0e462d3a2214a69bdc52d25
Walter Nelson <walter.john.nelson@gmail.com>
2012-06-02 19:58:34 -0400

    Fixed the FakeResponse class to be more general.
    Fixed error message whitespace in `Hotel::Helpers#parse_response`.

--- test/support/fake_response.rb
+++ test/support/fake_response.rb
@@ -1,7 +1,13 @@
 class FakeResponse
-  attr_reader :code, :body
-
   def initialize(options = {})
     options.each { |k, v| instance_variable_set("@#{k}", v) }
+  end
+
+  def method_missing(meth, *args, &blk)
+    if args.empty?
+      instance_variable_get("@#{meth}")
+    else
+      super
+    end
   end
 end
